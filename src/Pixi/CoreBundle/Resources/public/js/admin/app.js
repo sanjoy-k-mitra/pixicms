bbfebc5db51d73dc2638a771dedf7a01ee390e2e
sanjoy <sanjoy120@gmail.com>
2015-08-11 18:07:42 +0600

    removing lazy dependency modules

--- src/Pixi/CoreBundle/Resources/public/js/admin/app.js
+++ src/Pixi/CoreBundle/Resources/public/js/admin/app.js
@@ -2,7 +2,7 @@
  * Created by sanjoy on 6/26/15.
  */
 (function () {
-    var app = angular.module('pixiAdminApp', ['ui.router', 'oc.lazyLoad', 'ui.bootstrap'])
+    var app = angular.module('pixiAdminApp', ['ui.router', 'oc.lazyLoad', 'ui.bootstrap', 'sbAdminApp', 'toggle-switch', 'ngAnimate', 'ngCookies', 'ngResource', 'ngTouch', 'ngSanitize', 'chart.js'])
         .config(['$stateProvider', '$urlRouterProvider', '$ocLazyLoadProvider', function ($stateProvider, $urlRouterProvider, $ocLazyLoadProvider) {
             $ocLazyLoadProvider.config({
                 debug: true,
@@ -15,82 +15,22 @@
                 .state('dashboard', {
                     url: '/dashboard',
                     templateUrl: '/bundles/pixicore/template/admin/dashboard/main.html',
-                    resolve: {
-                        loadMyDirectives: function ($ocLazyLoad) {
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'sbAdminApp',
-                                    files: [
-                                        '/bundles/pixicore/scripts/directives/header/header.js',
-                                        '/bundles/pixicore/scripts/directives/header/header-notification/header-notification.js',
-                                        '/bundles/pixicore/scripts/directives/sidebar/sidebar.js',
-                                        '/bundles/pixicore/template/admin/resource/resource.js'
-                                    ]
-                                })
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'toggle-switch',
-                                    files: ["/bundles/pixicore/lib/angular-toggle-switch/angular-toggle-switch.min.js",
-                                        "/bundles/pixicore/lib/angular-toggle-switch/angular-toggle-switch.css"
-                                    ]
-                                })
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngAnimate',
-                                    files: ['/bundles/pixicore/lib/angular-animate/angular-animate.js']
-                                })
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngCookies',
-                                    files: ['/bundles/pixicore/lib/angular-cookies/angular-cookies.js']
-                                })
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngResource',
-                                    files: ['/bundles/pixicore/lib/angular-resource/angular-resource.js']
-                                })
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngSanitize',
-                                    files: ['/bundles/pixicore/lib/angular-sanitize/angular-sanitize.js']
-                                })
-                            $ocLazyLoad.load(
-                                {
-                                    name: 'ngTouch',
-                                    files: ['/bundles/pixicore/lib/angular-touch/angular-touch.js']
-                                })
-                        }
-                    }
                 })
                 .state('dashboard.home',{
                     url:'/home',
                     controller: 'MainCtrl',
                     templateUrl:'/bundles/pixicore/template/admin/dashboard/home.html',
-                    resolve: {
-                        loadMyFiles:function($ocLazyLoad) {
-                            return $ocLazyLoad.load({
-                                name:'sbAdminApp',
-                                files:[
-                                    '/bundles/pixicore/scripts/controllers/main.js',
-                                    '/bundles/pixicore/scripts/directives/timeline/timeline.js',
-                                    '/bundles/pixicore/scripts/directives/notifications/notifications.js',
-                                    '/bundles/pixicore/scripts/directives/chat/chat.js',
-                                    '/bundles/pixicore/scripts/directives/dashboard/stats/stats.js'
-                                ]
-                            })
-                        }
-                    }
                 })
                 .state('dashboard.permission', {
-                    template:"<resource-list name='Permissions' endpoint='/api/permission/'/>",
+                    template:"<resource-list name='permission' title='Permissions' endpoint='/api/permission/'/>",
                     url:'/permissions'
                 })
                 .state('dashboard.role', {
-                    template:"<resource-list name='Roles' endpoint='/api/userRole/'/>",
+                    template:"<resource-list name='role' title='Roles' endpoint='/api/userRole/'/>",
                     url:'/userRoles'
                 })
                 .state('dashboard.user', {
-                    template:"<resource-list name='Users' endpoint='/api/user/'/>",
+                    template:"<resource-list name='user' title='Users' endpoint='/api/user/'/>",
                     url:'/users'
                 })
                 .state('dashboard.form',{
@@ -109,21 +49,6 @@
                     templateUrl:'/bundles/pixicore/template/admin/chart.html',
                     url:'/chart',
                     controller:'ChartCtrl',
-                    resolve: {
-                        loadMyFile:function($ocLazyLoad) {
-                            return $ocLazyLoad.load({
-                                name:'chart.js',
-                                files:[
-                                    '/bundles/pixicore/lib/angular-chart.js/dist/angular-chart.min.js',
-                                    '/bundles/pixicore/lib/angular-chart.js/dist/angular-chart.css'
-                                ]
-                            }),
-                                $ocLazyLoad.load({
-                                    name:'sbAdminApp',
-                                    files:['/bundles/pixicore/scripts/controllers/chartContoller.js']
-                                })
-                        }
-                    }
                 })
                 .state('dashboard.table',{
                     templateUrl:'/bundles/pixicore/template/admin/table.html',
