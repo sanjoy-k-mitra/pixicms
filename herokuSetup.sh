79f2b056993fee4f25832dc577fb87e8d86068ca
sanjoy <sanjoy120@gmail.com>
2015-08-31 09:14:26 +0600

    update

--- herokuSetup.sh
+++ herokuSetup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 php app/console assetic:dump --env=prod --no-debug
 php app/console doctrine:schema:create
-bin/heroku-php-apache2 web/
 php app/console doctrine:fixtures:load
+bin/heroku-php-apache2 web/
