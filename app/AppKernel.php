7287697285a4e2c4d058fc6bb8455666f32af4cc
sanjoy <sanjoy120@gmail.com>
2015-08-31 14:16:34 +0600

    LoyaltyManagerBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -17,7 +17,6 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Pixi\CoreBundle\PixiCoreBundle(),
-            new Pixi\EcommerceBundle\PixiEcommerceBundle(),
             new Pixi\LoyaltyManagerBundle\PixiLoyaltyManagerBundle(),
         );
 
--- herokuSetup.sh
+++ herokuSetup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
 php app/console assetic:dump --env=prod --no-debug
-php app/console doctrine:schema:create
-php app/console doctrine:fixtures:load
+php app/console doctrine:schema:update --force
 bin/heroku-php-apache2 web/
