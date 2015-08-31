50f78842c685eac99d970f40eb7258a6c1005497
sanjoy <sanjoy120@gmail.com>
2015-08-31 08:33:42 +0600

    update

---
+++ src/Pixi/CoreBundle/HerokuDatabase.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: sanjoy
+ * Date: 8/31/15
+ * Time: 8:27 AM
+ */
+
+namespace Pixi\CoreBundle;
+
+use Composer\S\Event;
+
+class HerokuDatabase
+{
+    public static function populateEnvironment(Event $event)
+    {
+        $url = getenv("DATABASE_URL");
+
+        if ($url) {
+            $url = parse_url($url);
+            putenv("DATABASE_HOST={$url['host']}");
+            putenv("DATABASE_USER={$url['user']}");
+            putenv("DATABASE_PASSWORD={$url['pass']}");
+            $db = substr($url['path'],1);
+            putenv("DATABASE_NAME={$db}");
+        }
+
+        $io = $event->getIO();
+
+        $io->write("DATABASE_URL=".getenv("DATABASE_URL"));
+    }
+}
