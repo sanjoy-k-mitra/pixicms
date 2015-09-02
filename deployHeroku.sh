e08c279290d7a5a52138e46c328edef352911ce0
sanjoy <sanjoy120@gmail.com>
2015-09-02 06:12:34 +0600

    pixi-resource

--- deployHeroku.sh
+++ deployHeroku.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
+php app/console assetic:dump --env=prod --no-debug
 php app/console doctrine:schema:update --force
 bin/heroku-php-apache2 web/
--- herokuSetup.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-php app/console assetic:dump --env=prod --no-debug
-php app/console doctrine:schema:update --force
-bin/heroku-php-apache2 web/
